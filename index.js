e36acda5132a231205a5a31d55a0a32f16acdf05
Shiv Sharma <shivsharmavictory@gmail.com>
2022-10-08 12:49:59 +0530

    added the routes

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Preparing Project');
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const routes = require('./routes/index');
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', routes);
+
+
+app.listen(PORT, (err) => {
+    console.log(`Server is listening at port ${PORT}`);
+});
